059cd308a426b075c93bcc8308da1d810088f561
Yusuf Khasbulatov <khashashin@gmail.com>
2019-07-17 02:53:05 +0200

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    @ViewChild(IonSlides) slider: IonSlides;
+    segment = 0;
 
-  constructor() {}
+    constructor() {}
 
+    segmentChanged() {
+        this.slider.slideTo(this.segment);
+    }
+
+    async slideChanged() {
+        this.segment = await this.slider.getActiveIndex();
+    }
 }
